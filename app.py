4654f0b0325a058503c51083d0f66f4b45838017
Antoine Ledoux <Yoshcakes@Antoines-iMac.local>
2018-05-02 14:31:27 -0400

    better tiling for skills

--- app.py
+++ app.py
@@ -1,42 +1,67 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Actor
+from flask import Flask, flash, render_template, request, redirect, url_for, abort
+# from flask_mongoengine import MongoEngine
+# from flask_security import Security, MongoEngineUserDatastore, UserMixin, RoleMixin, login_required
+# from flask_login import login_user
 
-import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from database_setup import Base, Actor
+
+# import os
 
 ###### START FLASK APP AND CONNECT TO DATABASE
 app = Flask(__name__)
-engine = create_engine('sqlite:///actors.db')
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+# engine = create_engine('sqlite:///actors.db')
+# Base.metadata.bind = engine
+# DBSession = sessionmaker(bind=engine)
+# session = DBSession()
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
-
+    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
 
 ############### ACTOR PAGE
 @app.route('/actor/', methods=['GET', 'POST'])
 def show_actor_profile():
     return render_template('actorprofile.html')
 
-
 @app.route('/jobswall/', methods=['GET', 'POST'])
 def show_jobs_wall():
     return render_template('jobswall.html')
 
+@app.route('/skills/language/', methods=['GET', 'POST'])
+def skills_language():
+    return render_template('skills_language.html')
+
+@app.route('/skills/sport/', methods=['GET', 'POST'])
+def skills_sport():
+    return render_template('skills_sport.html')
+
+@app.route('/skills/acting/', methods=['GET', 'POST'])
+def skills_acting():
+    return render_template('skills_acting.html')
+
+@app.route('/skills/measurements/', methods=['GET', 'POST'])
+def skills_measurements():
+    return render_template('skills_measurements.html')
+
+@app.route('/skills/other/', methods=['GET', 'POST'])
+def skills_other():
+    return render_template('skills_other.html')
 
 ############### DIRECTOR SEARCH PAGE
-@app.route('/postgig/', methods=['GET', 'POST'])
+@app.route('/postjobs/', methods=['GET', 'POST'])
 def post_gig():
-    return render_template('postgig.html')
+    return render_template('postjobs.html')
 
 @app.route('/searchactors/')
 def search_actors():
@@ -44,5 +69,4 @@ def search_actors():
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run(host='127.0.0.1', port=8080)
+    app.run(host='127.0.0.1', port=8080, debug=True)
